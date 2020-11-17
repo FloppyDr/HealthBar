42a525914bc6747e0c06b141fa163f1ee4247924
FloppyDr <DimaAlexandrov@yandex.ru>
2020-11-18 02:30:24 +0300

    Init

---
+++ Assets/Scripts/HealthValueChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class HealthValueChanger : MonoBehaviour
+{
+   [SerializeField] private Slider _healthBar;
+
+    public void ChangeHp(float hp)
+    {
+        _healthBar.DOValue(_healthBar.value + hp, 0.5f, true);
+    }
+}
